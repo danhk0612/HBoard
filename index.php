7dfdc11980318f05823c4187cc29da151157924d
HaeKwang Dan <gorhkd0612@gmail.com>
2017-06-28 21:34:12 +1000

    Setting Init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once("/config/config.php");
+require_once("/lib/db.php");
+$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
+$sql = "";
+// $result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    1
+</body>
+</html>
